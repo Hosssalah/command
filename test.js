52ea3ed50a850ad67b8be330de54d2ef012c26e9
Hossam Salah <hossam.salah@arma.com.eg>
2022-07-29 16:22:21 +0200

    first app

---
+++ test.js
@@ -0,0 +1,77 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function showtime() {
+
+//   var datee = new Date();
+//   console.log(datee);
+
+//   var hourss = datee.getHours();
+//   var minutess = datee.getMinutes();
+//   var secondd = datee.getSeconds();
+
+
+//   (hourss < 10) ? "0" + hourss : hourss;
+//   (minutess < 10) ? "0" + minutess : minutess;
+//   (secondd < 10) ? "0" + secondd : secondd;
+
+//   var timee =  hourss + ":" + minutess + ":" + secondd;
+
+//   document.getElementById("myclockdisplay").innerHTML = timee
+//   setTimeout(showtime, 1000)
+
+// }
+// showtime()
+
+
+
+
+
+
+
+
+// function showtime() {
+//  var date = new Date();
+//  console.log(date)
+
+
+//  var Hourss = date.getHours();
+//  var Minutess = date.getMinutes();
+//  var Secondd = date.getSeconds();
+
+//  if(Hourss > 12) {
+//     Hourss = Hourss - 12;
+
+//  }
+
+//  Hourss = (Hourss < 10) ? "0" + Hourss : Hourss;
+//  Minutess = (Minutess < 10) ? "0" + Minutess : Minutess;
+//  Secondd = (Secondd < 10) ? "0" + Secondd : Secondd;
+
+
+// var Time = Hourss + ":" + Minutess + ":" + Secondd; 
+//  document.getElementById("myclockdisplay").innerHTML = Time;
+//  setTimeout(showtime, 1000)
+
+// }
+// showtime();
+
+
+
+// var Date = new Date(). getDate();
+// console.log(Date)
